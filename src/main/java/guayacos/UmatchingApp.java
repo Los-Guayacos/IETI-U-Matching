5d986d17299b2dfd45d51880977ec79de6f6dc0b
David Coronado <david.coronado@escuelaing.edu.co>
2021-09-22 19:12:46 -0500

    add basic configurations Spring App

---
+++ src/main/java/guayacos/UmatchingApp.java
@@ -0,0 +1,14 @@
+package guayacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmatchingApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(UmatchingApp.class,args);
+
+    }
+}

aaee2c5afe4b949862250b55a10d1c63cfe38052
Miguel Angel Fuquene Arias <migueplay_10@hotmail.com>
2021-09-23 07:14:36 -0500

    Despliegue heroku, se agrego Procfile y variables de entorno

--- src/main/java/guayacos/UmatchingApp.java
+++ src/main/java/guayacos/UmatchingApp.java
@@ -9,6 +9,7 @@ public class UmatchingApp
     public static void main( String[] args )
     {
         SpringApplication.run(UmatchingApp.class,args);
+        System.out.println("The app is running...");
 
     }
 }

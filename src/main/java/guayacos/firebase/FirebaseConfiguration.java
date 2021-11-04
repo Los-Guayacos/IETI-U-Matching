64f97f9a28bf11354324bc64a360f160a7c9cbb7
Miguel Angel Fuquene Arias <migueplay_10@hotmail.com>
2021-11-03 21:47:36 -0500

    Firebase added

---
+++ src/main/java/guayacos/firebase/FirebaseConfiguration.java
@@ -0,0 +1,36 @@
+package guayacos.firebase;
+
+import java.io.FileInputStream;
+
+import javax.annotation.PostConstruct;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import edu.escuelaing.arsw.chat.components.ChatListener;
+
+@Service
+public class FirebaseConfiguration {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            System.out.println("Initalizing firebase...");
+            FileInputStream serviceAccount = new FileInputStream("serviceAccount.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://u-matching-default-rtdb.firebaseio.com/").build();
+
+            if(FirebaseApp.getApps().isEmpty()){
+                FirebaseApp.initializeApp(options);
+            }
+
+            ChatListener chatListener = new ChatListener();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
